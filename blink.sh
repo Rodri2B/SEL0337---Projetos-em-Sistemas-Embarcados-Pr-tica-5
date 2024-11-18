bc59f1a683de03ca72e44c403728af0a57390c18
Rodri2B <rodrigocbastos@usp.br>
2024-11-18 13:47:27 -0300

    adicionando código a ser executado na parada do serviço

--- blink.sh
+++ blink.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo 18 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio18/direction
+echo 12 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio12/direction
 while [ 1 ]
 	do
-		echo 1 > /sys/class/gpio/gpio18/value
+		echo 1 > /sys/class/gpio/gpio12/value
 		sleep 0.2s
-		echo 0 > /sys/class/gpio/gpio18/value
+		echo 0 > /sys/class/gpio/gpio12/value
 		sleep 0.2s
 	done
---
+++ semaforo_on_stop.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+red_pin=9 #define o pino do led vermelho
+green_pin=10 #define o pino do led verde
+blue_pin=11 #define o pino do led azul
+
+
+if __name__ == '__main__':
+
+    try:
+        GPIO.setmode(GPIO.BCM) #configurando o modo do gpio
+
+	GPIO.setup(red_pin , GPIO.OUT) #configurando o pino do led vermelho como saida
+    	GPIO.setup(green_pin , GPIO.OUT) #configurando o pino do led verde como saida
+    	GPIO.setup(blue_pin , GPIO.OUT) #configurando o pino do led azul como saida
+
+
+    	GPIO.output(red_pin , GPIO.LOW) #desligando led vermelho
+    	GPIO.output(red_pin , GPIO.LOW) #desligando led verde
+    	GPIO.output(red_pin , GPIO.LOW) #desligando led azul
+	
+	#loop para implementar a função do semaforo
+    	while (True):
+        	#liagndo luz vermelha
+        	GPIO.output(red_pin , GPIO.HIGH) #ligando led vermelho
+        	GPIO.output(red_pin , GPIO.LOW) #desligando led verde
+        	time.sleep(1)
+        	#ligando luz amarela
+        	GPIO.output(green_pin , GPIO.LOW) #ligando led verde
+        	time.sleep(1)
+        	#ligando luz verde
+        	GPIO.output(red_pin , GPIO.LOW) #desligando led vermelho
+        	time.sleep(1)
+        
+    
+    except KeyboardInterrupt: # interrupção do teclado (Ctrl+C).
+        print("Processo interrompido.")
+    finally:
+        GPIO.cleanup() # Limpa a configuração dos pinos GPIO ao encerrar o script.
+
