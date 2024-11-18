475f2b9b8da7991da242170ee6e3bfb7f3929838
Rodri2B <rodrigocbastos@usp.br>
2024-11-18 16:16:15 -0300

    codigos alterados

--- prat3_proj2_1.py
+++ prat3_proj2_1.py
@@ -6,10 +6,13 @@ if __name__ == '__main__':
 
     try:
         GPIO.setmode(GPIO.BCM) #configurando o modo do gpio
-
+	
         pwm_pin  = 12 #definindo pino de pwm
-
-        GPIO.setup(pwm_pin , GPIO.OUT) #configurando o pino 12 como saida
+	
+	other_pin = 10
+	other_pin = 9
+	GPIO.setup(other_pin , GPIO.OUT) #configurando o pino 12 como saida
+        GPIO.setup(other_pin2 , GPIO.OUT) #configurando o pino 12 como saida
 
         pwm = GPIO.PWM(pwm_pin , 5) #configuando o pwm no pino 12 com 5Hz
         pwm.start (0) #inicializando pwm com duty cycle 0
--- semaforo_on_stop.py
+++ semaforo_on_stop.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-from time import sleep
+import time
 
 red_pin=9 #define o pino do led vermelho
 green_pin=10 #define o pino do led verde
@@ -9,29 +9,31 @@ blue_pin=11 #define o pino do led azul
 if __name__ == '__main__':
 
     try:
+        GPIO.cleanup() # Limpa a configuração dos pinos GPIO      
+ 
         GPIO.setmode(GPIO.BCM) #configurando o modo do gpio
 
-	GPIO.setup(red_pin , GPIO.OUT) #configurando o pino do led vermelho como saida
-    	GPIO.setup(green_pin , GPIO.OUT) #configurando o pino do led verde como saida
-    	GPIO.setup(blue_pin , GPIO.OUT) #configurando o pino do led azul como saida
+        GPIO.setup(red_pin , GPIO.OUT) #configurando o pino do led vermelho como saida
+        GPIO.setup(green_pin , GPIO.OUT) #configurando o pino do led verde como saida
+        GPIO.setup(blue_pin , GPIO.OUT) #configurando o pino do led azul como saida
 
 
-    	GPIO.output(red_pin , GPIO.LOW) #desligando led vermelho
-    	GPIO.output(red_pin , GPIO.LOW) #desligando led verde
-    	GPIO.output(red_pin , GPIO.LOW) #desligando led azul
-	
-	#loop para implementar a função do semaforo
-    	while (True):
-        	#liagndo luz vermelha
-        	GPIO.output(red_pin , GPIO.HIGH) #ligando led vermelho
-        	GPIO.output(red_pin , GPIO.LOW) #desligando led verde
-        	time.sleep(1)
-        	#ligando luz amarela
-        	GPIO.output(green_pin , GPIO.LOW) #ligando led verde
-        	time.sleep(1)
-        	#ligando luz verde
-        	GPIO.output(red_pin , GPIO.LOW) #desligando led vermelho
-        	time.sleep(1)
+        GPIO.output(red_pin , GPIO.LOW) #desligando led vermelho
+        GPIO.output(red_pin , GPIO.LOW) #desligando led verde
+        GPIO.output(red_pin , GPIO.LOW) #desligando led azul
+
+        #loop para implementar a função do semaforo
+        while (True):
+            #liagndo luz vermelha
+            GPIO.output(red_pin , GPIO.HIGH) #ligando led vermelho
+            GPIO.output(green_pin , GPIO.LOW) #desligando led verde
+            time.sleep(1)
+            #ligando luz amarela
+            GPIO.output(green_pin , GPIO.HIGH) #ligando led verde
+            time.sleep(1)
+            #ligando luz verde
+            GPIO.output(red_pin , GPIO.LOW) #desligando led vermelho
+            time.sleep(1)
         
     
     except KeyboardInterrupt: # interrupção do teclado (Ctrl+C).
